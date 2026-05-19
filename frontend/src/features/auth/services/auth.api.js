9609a97b7c53960ef2f2c35c276a8828180bed84
SAPTARSHI BHUNIA <169096401+Shadowblue3@users.noreply.github.com>
2026-05-19 10:45:45 +0530

    url error fixed

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 //This is an axios instance and now can be used for any functions, without repeting the same lines of code
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://talvo-ai-resume-analyzer.onrender.com",
     withCredentials: true
 })
 
